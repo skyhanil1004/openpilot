802b4ef32c1aed0756e7bc743e943a96c3206d17
Comma Device <device@comma.ai>
2022-04-08 19:35:56 +0900

    Tune.py fix interface value

--- selfdrive/car/landrover/interface.py
+++ selfdrive/car/landrover/interface.py
@@ -32,6 +32,12 @@ class CarInterface(CarInterfaceBase):
     # added to selfdrive/test/test_routes, we can remove it from this list.
     ret.dashcamOnly = False #candidate in {CAR.KIA_OPTIMA_H, CAR.ELANTRA_GT_I30}
 
+    ret.steerRatio = 15.5
+    ret.steerActuatorDelay = 0.02      # 0.05 -> 0.065 12/11 modify org 0.1 -> 0.025 then good straight
+    ret.lateralTuning.pid.kf = 0.00005   # full torque for 10 deg at 80mph means 0.00007818594
+
+    ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[9., 20.], [9., 20.]]
+    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01,0.02], [0.0005,0.001]]
 
     ret.steerRateCost = 0.5
     ret.steerLimitTimer = 0.4
@@ -45,15 +51,8 @@ class CarInterface(CarInterfaceBase):
     ret.stopAccel = 0.0
     ret.longitudinalActuatorDelayUpperBound = 1.0 # s
 
-    ret.steerRatio = 15.5
-    ret.steerActuatorDelay = 0.02      # 0.05 -> 0.065 12/11 modify org 0.1 -> 0.025 then good straight
-    ret.lateralTuning.pid.kf = 0.00005   # full torque for 10 deg at 80mph means 0.00007818594
     ret.mass = 2500. + STD_CARGO_KG  # kg curb weight Pacifica Hybrid 2017
     ret.wheelbase =  2.922  # in meters for rangerover vogue  2017
-    # Values from optimizer
-    #ret.steerRatio = 11.2
-    ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[9., 20.], [9., 20.]]
-    ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.01,0.02], [0.0005,0.001]]
     ret.minSteerSpeed = 1.8    # m/s
     ret.minEnableSpeed = -1.   # enable is done by stock ACC, so ignore this
 
