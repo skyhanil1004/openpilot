7bfc0977a051a5be8297a7c2e0ac737fcf45ad72
Comma Device <device@comma.ai>
2022-04-20 20:59:54 +0900

    Comm Issue fix

--- selfdrive/car/landrover/carcontroller.py
+++ selfdrive/car/landrover/carcontroller.py
@@ -1,15 +1,10 @@
 from cereal import car
-from common.realtime import DT_CTRL
-from common.numpy_fast import clip, interp
-from common.conversions import Conversions as CV
 from selfdrive.car import apply_std_steer_torque_limits, apply_toyota_steer_torque_limits
 from selfdrive.car.landrover.landrovercan import create_lkas_command, create_lkas_hud
 from selfdrive.car.landrover.values import CarControllerParams, CAR, STATIC_MSGS
 from opendbc.can.packer import CANPacker
 
-VisualAlert = car.CarControl.HUDControl.VisualAlert
-LongCtrlState = car.CarControl.Actuators.LongControlState
-
+"""
 def process_hud_alert(enabled, fingerprint, visual_alert, left_lane,
                       right_lane, left_lane_depart, right_lane_depart):
   sys_warning = (visual_alert in (VisualAlert.steerRequired, VisualAlert.ldw))
@@ -32,13 +27,14 @@ def process_hud_alert(enabled, fingerprint, visual_alert, left_lane,
     right_lane_warning = 1 if fingerprint in (CAR.GENESIS_G90, CAR.GENESIS_G80) else 2
 
   return sys_warning, sys_state, left_lane_warning, right_lane_warning
+"""
 
 class CarController():
   def __init__(self, dbc_name, CP, VM):
     self.CP = CP
     self.apply_steer_last = 0
     self.ccframe = 0
-    self.p = CarControllerParams
+    #self.p = CarControllerParams
     self.prev_frame = -1
     self.hud_count = 0
     self.lkascnt = 0
@@ -49,20 +45,20 @@ class CarController():
     self.packer = CANPacker(dbc_name)
 
   def update(self, enabled, CS, actuators, pcm_cancel_cmd, hud_alert):
-               # hud_speed,left_lane, right_lane, left_lane_depart, right_lane_depart):
+    # this seems needed to avoid steering faults and to force the sync with the EPS counter
     frame = CS.lkas_counter
     if self.prev_frame == frame:
       return car.CarControl.Actuators.new_message(), []
 
     # Steering Torque
     new_steer = int(round(actuators.steer * 1024))
-    #apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.p)
-    #apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, self.p)
     apply_steer = apply_toyota_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorqueEps, CarControllerParams)
+
     self.steer_rate_limited = new_steer != apply_steer
 
     # disable when temp fault is active, or below LKA minimum speed
-    lkas_active = enabled and not CS.out.steerFaultTemporary and CS.out.vEgo >= CS.CP.minSteerSpeed
+    #lkas_active = enabled and not CS.out.steerFaultTemporary and CS.out.vEgo >= CS.CP.minSteerSpeed
+    lkas_active = enabled
 
     if CS.out.leftTurn or CS.out.rightTurn:
       apply_steer = 0
@@ -77,18 +73,16 @@ class CarController():
     for (addr, ecu, cars, bus, fr_step, vl) in STATIC_MSGS:
        if(frame % fr_step == 0):  # 25 0.25s period
            can_sends.append([addr, bus, vl, 0])
-    """
-    if (self.ccframe % 20 == 0):
-       new_msg = create_lkas_hud(self.packer, CS.lkas_status, left_lane, right_lane, left_lane_depart, right_lane_depart )
-       can_sends.append(new_msg)
-       self.hud_count += 1
-    """
 
-    if (self.ccframe % 2 == 0):
-       new_msg = create_lkas_command(self.packer, CS.lkas_run, self.lkascnt, int(apply_steer))
-       self.lkascnt += 1
-       #cloudlog.warning("LANDROVER LKAS (%s)", new_msg)
-       can_sends.append(new_msg)
+    if (self.ccframe % 20 == 0):
+       #new_msg = create_lkas_hud(self.packer, CS.lkas_status, left_lane, right_lane, left_lane_depart, right_lane_depart )
+       #can_sends.append(new_msg)
+       self.hud_count += 1
+
+    new_msg = create_lkas_command(self.packer, CS.lkas_run, self.lkascnt, int(apply_steer))
+    can_sends.append(new_msg)
+    self.lkascnt += 1
+    #cloudlog.warning("LANDROVER LKAS (%s)", new_msg)
 
     self.ccframe += 1
     self.prev_frame = frame
--- selfdrive/car/landrover/carstate.py
+++ selfdrive/car/landrover/carstate.py
@@ -1,11 +1,10 @@
 import copy
 from cereal import car
-from selfdrive.car.landrover.values import DBC, STEER_THRESHOLD
-from selfdrive.car.interfaces import CarStateBase
+from common.conversions import Conversions as CV
 from opendbc.can.parser import CANParser
 from opendbc.can.can_define import CANDefine
-#from selfdrive.config import Conversions as CV
-from common.conversions import Conversions as CV
+from selfdrive.car.interfaces import CarStateBase
+from selfdrive.car.landrover.values import DBC, STEER_THRESHOLD
 
 
 class CarState(CarStateBase):
@@ -18,14 +17,14 @@ class CarState(CarStateBase):
 
     ret = car.CarState.new_message()
 
-    self.frame = int(cp.vl["LKAS_RUN"]["COUNTER"])
+    self.frame = int(cp.vl["THROTTLE"]["COUNTER"])
 
     ret.doorOpen = (cp.vl["SEAT_BELT"]["SEAT_BELT_DRIVER"]  == 0)
     ret.seatbeltUnlatched = (cp.vl["SEAT_BELT"]["SEAT_BELT_DRIVER"]  == 0)
 
     ret.brakePressed = (cp.vl["CRUISE_CONTROL"]['DRIVER_BRAKE'] == 1)
-    ret.gas = 0
     ret.brake = 0
+    ret.gas = 0
     ret.gasPressed = ret.gas > 1e-3
 
     #ret.wheelSpeeds = (cp.vl["SPEED_01"]["SPEED01"] + cp.vl["SPEED_02"]["SPEED02"]) / 2.
@@ -70,61 +69,62 @@ class CarState(CarStateBase):
   def get_can_parser(CP):
     signals = [
       # sig_name, sig_address, default
-      ("STEER_RATE00", "EPS_00", 0),
-      ("STEER_ANGLE01", "EPS_01", 0),
-      ("STEER_SPEED01", "EPS_01", 0),
-      ("STEER_TORQUE_DRIVER02", "EPS_02", 0),
-      ("STEER_TORQUE_MOTOR02", "EPS_02", 0),
-      ("STEER_TORQUE_DRIVER03", "EPS_03", 0),
-      ("STEER_TQ", "EPS_04", 0),
-      ("GEAR_SHIFT", "GEAR_PRND", 0),
-      ("CRUISE_ON", "CRUISE_CONTROL", 0),
-      ("DRIVER_BRAKE", "CRUISE_CONTROL", 0),
-      ("SPEED_CRUISE_RESUME", "CRUISE_CONTROL", 1),
-      ("ACCELATOR_DRIVER", "ACCELATOR_DRIVER", 0),
-      ("SEAT_BELT_DRIVER", "SEAT_BELT", 0),
-      ("RIGHT_TURN", "TURN_SIGNAL", 0),
-      ("LEFT_TURN", "TURN_SIGNAL", 0),
-      ("LEFT_BLINK", "TURN_SIGNAL", 0),
-      ("RIGHT_BLINK", "TURN_SIGNAL", 0),
-      ("SPEED01", "SPEED_01", 0),
-      ("SPEED02", "SPEED_02", 0),
+      ("COUNTER", "THROTTLE"),
+      ("STEER_RATE00", "EPS_00"),
+      ("STEER_ANGLE01", "EPS_01"),
+      ("STEER_SPEED01", "EPS_01"),
+      ("STEER_TORQUE_DRIVER02", "EPS_02"),
+      ("STEER_TORQUE_MOTOR02", "EPS_02"),
+      ("STEER_TORQUE_DRIVER03", "EPS_03"),
+      ("STEER_TQ", "EPS_04"),
+      ("GEAR_SHIFT", "GEAR_PRND"),
+      ("CRUISE_ON", "CRUISE_CONTROL"),
+      ("DRIVER_BRAKE", "CRUISE_CONTROL"),
+      ("SPEED_CRUISE_RESUME", "CRUISE_CONTROL"),
+      ("ACCELATOR_DRIVER", "ACCELATOR_DRIVER"),
+      ("SEAT_BELT_DRIVER", "SEAT_BELT"),
+      ("RIGHT_TURN", "TURN_SIGNAL"),
+      ("LEFT_TURN", "TURN_SIGNAL"),
+      ("LEFT_BLINK", "TURN_SIGNAL"),
+      ("RIGHT_BLINK", "TURN_SIGNAL"),
+      ("SPEED01", "SPEED_01"),
+      ("SPEED02", "SPEED_02"),
       #("LEFT_ALERT_1", "LEFT_ALERT", 0),
       #("RIGHT_ALERT_1", "RIGHT_ALERT", 0),
-      ( "COUNTER", "LKAS_RUN", -1),
-      ("WHEEL_SPEED_FR", "SPEED_04", 0),
-      ("WHEEL_SPEED_FL", "SPEED_04", 0),
-      ("WHEEL_SPEED_RR", "SPEED_03", 0),
-      ("WHEEL_SPEED_RL", "SPEED_03", 0),
-      ("GREEN2WHITE_RIGHT", "LKAS_STATUS", 1),
-      ("GREEN2WHITE_LEFT", "LKAS_STATUS", 1),
-      ("FRONT_CAR_DISTANCE", "LKAS_HUD_STAT", 0),
-      ("LADAR_DISTANCE", "LKAS_HUD_STAT", 0),
-      ("HIBEAM", "HEAD_LIGHT", 0),
+      #( "COUNTER", "LKAS_RUN"),
+      ("WHEEL_SPEED_FR", "SPEED_04"),
+      ("WHEEL_SPEED_FL", "SPEED_04"),
+      ("WHEEL_SPEED_RR", "SPEED_03"),
+      ("WHEEL_SPEED_RL", "SPEED_03"),
+      ("GREEN2WHITE_RIGHT", "LKAS_STATUS"),
+      ("GREEN2WHITE_LEFT", "LKAS_STATUS"),
+      ("FRONT_CAR_DISTANCE", "LKAS_HUD_STAT"),
+      ("LADAR_DISTANCE", "LKAS_HUD_STAT"),
+      ("HIBEAM", "HEAD_LIGHT"),
     ]
 
     # It's considered invalid if it is not received for 10x the expected period (1/f).
     checks = [
       # sig_address, frequency
-      ("EPS_00", 0),
-      ("EPS_01", 0),
-      ("EPS_02", 0),
-      ("EPS_03", 0),
-      ("EPS_04",  0),
-      ("SPEED_01", 0),
-      ("SPEED_02", 0),
-      ("SPEED_03", 0),
-      ("SPEED_04", 0),
-      ("CRUISE_CONTROL", 1),
-      ("SEAT_BELT", 0),
-      ("LKAS_HUD_STAT", 0),
-      ("HEAD_LIGHT", 0),
-      ("LKAS_STATUS", 0),
-      ("GEAR_PRND", 100),
-      ("TURN_SIGNAL", 0),
+      ("EPS_00", 100),
+      ("EPS_01", 100),
+      ("EPS_02", 100),
+      ("EPS_03", 100),
+      ("EPS_04", 100),
+      ("SPEED_01", 100),
+      ("SPEED_02", 100),
+      ("SPEED_03", 100),
+      ("SPEED_04", 100),
+      ("CRUISE_CONTROL", 100),
       ("SEAT_BELT", 100),
-      ("ACCELATOR_DRIVER", 0),
-      ( "LKAS_RUN", 0),
+      ("LKAS_HUD_STAT", 00),
+      ("HEAD_LIGHT", 100),
+      ("LKAS_STATUS", 100),
+      ("GEAR_PRND", 100),
+      ("TURN_SIGNAL", 10),
+      ("SEAT_BELT", 10),
+      ("ACCELATOR_DRIVER", 10),
+      ( "THROTTLE", 0),
     ]
 
     return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 0)
@@ -134,16 +134,16 @@ class CarState(CarStateBase):
       signals = [
         # sig_name, sig_address, default
         # TODO read in all the other values
-        ( "ALLFFFF", "LKAS_RUN", 0),
-        ( "A1", "LKAS_RUN", 0),
-        ( "HIGH_TORQ", "LKAS_RUN", 0),
-        ( "ALL11", "LKAS_RUN", 0),
-        ( "COUNTER", "LKAS_RUN", -1),
-        ( "STEER_TORQ", "LKAS_RUN", 0),
-        ( "LKAS_GREEN", "LKAS_RUN", 0),
+        ( "ALLFFFF", "LKAS_RUN"),
+        ( "A1", "LKAS_RUN"),
+        ( "HIGH_TORQ", "LKAS_RUN"),
+        ( "ALL11", "LKAS_RUN"),
+        ( "COUNTER", "LKAS_RUN"),
+        ( "STEER_TORQ", "LKAS_RUN"),
+        ( "LKAS_GREEN", "LKAS_RUN"),
       ]
       checks = [
-         ( "LKAS_RUN", 0),
+         ( "LKAS_RUN", 1),
       ]
 
       return CANParser(DBC[CP.carFingerprint]["pt"], signals, checks, 2)
